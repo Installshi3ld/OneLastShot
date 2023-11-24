f3de44ae5d6d12ce5d22489cd2a66ab66f2fa670
Installshi3ld <thomas.wawrysko@gmail.com>
2023-11-24 21:56:39 +0100

    Bar

---
+++ Assets/Thomas/S_MovingBackground.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_MovingBackground : MonoBehaviour
+{
+    private float lenght, startpos;
+    public GameObject cam;
+    public SpriteRenderer backgroundImage;
+    public float parallaxEffect;
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        lenght = backgroundImage.bounds.size.x;
+    }
+    
+    private void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + lenght) startpos += lenght;
+        else if(temp < startpos - lenght) startpos -= lenght;
+        Debug.Log($"Temp: {temp}, lenght: {lenght}");
+    }
+}
