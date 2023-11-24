8e8b13b9753973d24219c12bdf023dedb9e9b64c
Installshi3ld <thomas.wawrysko@gmail.com>
2023-11-24 13:02:29 +0100

    Umbrella

---
+++ Assets/Thomas/S_UmbrellaController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_UmbrellaController : MonoBehaviour
+{
+    public float vitesseRotation = 5f;
+    private Vector3 positionInitiale;
+    private float totalRotation = 0f;
+
+    void Start()
+    {
+        positionInitiale = Input.mousePosition;
+    }
+
+    void Update()
+    {
+        float rotationX = (Input.mousePosition.x - positionInitiale.x) * vitesseRotation;
+
+        totalRotation += rotationX * Time.deltaTime;
+        totalRotation = Mathf.Clamp(totalRotation, -30f, 30f);
+
+        Vector3 newRotation = new Vector3(0f, 0f, -totalRotation);
+
+        transform.rotation = Quaternion.Euler(newRotation);
+
+        positionInitiale = Input.mousePosition;
+    }
+}
