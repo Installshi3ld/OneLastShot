5f635c3b18770014faa27674fe92d27af7cc22c4
Installshi3ld <thomas.wawrysko@gmail.com>
2023-11-24 14:54:37 +0100

    Update

---
+++ Assets/Thomas/Data/S_CleannessData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_CleanessData", menuName = "S_Data/ClenessData")]
+public class S_CleannessData : S_Data<float>
+{
+    
+}
