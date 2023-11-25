c0246b47b7b585357f2bba0c7f3d10da5fe61ee6
Installshi3ld <thomas.wawrysko@gmail.com>
2023-11-25 03:23:39 +0100

    Adding Mesh collider player

--- Assets/Thomas/S_ItemDropRotationBegin.cs
+++ Assets/Thomas/S_ItemDropRotationBegin.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class S_ItemDropRotationBegin : MonoBehaviour
 {
+    public float min = 5f, max = 10f;
     private void Start()
     {
         gameObject.GetComponent<Rigidbody2D>().AddTorque(7f);
